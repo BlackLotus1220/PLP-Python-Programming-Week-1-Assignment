5a5a5321551539ba72bfaa4da94b10306ff13828
Elizabeth Wanjiku <wanjikuelizabeth654@gmail.com>
2024-08-20 15:57:52 +0300

    Week 1 .py Assignment

---
+++ user_input.py
@@ -0,0 +1,11 @@
+name= input("Please enter your name")
+age= int(input("Please enter your age"))
+location= input("Please enter your location")
+
+#get user info
+name= input("Please enter your name")
+age=int(input("Please enter your age"))
+location= input("Please enter your location")
+
+#print message
+print("Hello {name}, you are {age} years old and live in {location}.")
